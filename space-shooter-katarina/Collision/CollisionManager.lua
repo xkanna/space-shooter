dff7541b88dba6cdb5b8b9d39e0ee7879e750ffa
Katarina Veljkovic <40143205+xkanna@users.noreply.github.com>
2024-08-06 20:34:24 +0200

    Collision with quadrants added for optimisation

--- space-shooter-katarina/Collision/CollisionManager.lua
+++ space-shooter-katarina/Collision/CollisionManager.lua
@@ -8,45 +8,99 @@ local BulletSpawner = require("Shooting.BulletSpawner")
 local Bullet = require("Shooting.Bullet")
 local Collectable = require("Collectables.Collectable")
 
-local explosions = {} 
+local explosions = {}
 local collectables = {}
 
+function CollisionManager:init()
+    self.quadrants = {
+        bullets = {{}, {}, {}, {}},
+        enemies = {{}, {}, {}, {}},
+        collectables = {{}, {}, {}, {}}
+    }
+end
+
 function CollisionManager:checkCollisions(bullets, enemies, ship, dt)
-    local bulletsToRemove = {}
-    local enemiesToRemove = {}
-    
+    self:sortObjectsIntoQuadrants(bullets, enemies, collectables)
+
+    for i = 1, 4 do
+        self:checkQuadrantCollisions(i, ship, dt)
+    end
+
+    self:checkCollectableShipCollision(ship)
+    self:updateCollectables(collectables, ship, dt)
+    updateExplosions(dt)
+end
+
+function CollisionManager:sortObjectsIntoQuadrants(bullets, enemies, collectables)
+    self.quadrants.bullets = {{}, {}, {}, {}}
+    self.quadrants.enemies = {{}, {}, {}, {}}
+    self.quadrants.collectables = {{}, {}, {}, {}}
+
+    for _, bullet in ipairs(bullets) do
+        local quadrant = self:getQuadrant(bullet.x, bullet.y)
+        table.insert(self.quadrants.bullets[quadrant], bullet)
+    end
+
+    for _, enemy in ipairs(enemies) do
+        local quadrant = self:getQuadrant(enemy.x, enemy.y)
+        table.insert(self.quadrants.enemies[quadrant], enemy)
+    end
+
+    for _, collectable in ipairs(collectables) do
+        local quadrant = self:getQuadrant(collectable.x, collectable.y)
+        table.insert(self.quadrants.collectables[quadrant], collectable)
+    end
+end
+
+function CollisionManager:getQuadrant(x, y)
+    local stageWidth, stageHeight = Model.stage.stageWidth, Model.stage.stageHeight
+    if x < stageWidth / 2 then
+        if y < stageHeight / 2 then
+            return 1
+        else
+            return 3
+        end
+    else
+        if y < stageHeight / 2 then
+            return 2
+        else
+            return 4
+        end
+    end
+end
+
+function CollisionManager:checkQuadrantCollisions(quadrant, ship, dt)
+    local bullets = self.quadrants.bullets[quadrant]
+    local enemies = self.quadrants.enemies[quadrant]
+
     for i = #bullets, 1, -1 do
         local bullet = bullets[i]
-        
+
         for j = #enemies, 1, -1 do
             local enemy = enemies[j]
-            
+
             if self:checkBulletEnemyCollision(bullet, enemy) then
                 self:bulletAndEnemyCollided(bullet, enemy)
                 break
             end
         end
     end
-    
+
     for i = #enemies, 1, -1 do
         local enemy = enemies[i]
-        
+
         if self:checkShipEnemyCollision(ship, enemy) then
             self:shipAndEnemyCollided(ship, enemy)
             break
         end
     end
-    
-    self:checkCollectableShipCollision(ship)
-    self:updateCollectables(collectables, ship,  dt)
-    updateExplosions(dt)
 end
 
 function CollisionManager:checkBulletEnemyCollision(bullet, enemy)
     local dx = bullet.x - enemy.x
     local dy = bullet.y - enemy.y
     local distance = math.sqrt(dx * dx + dy * dy)
-    
+
     return distance < (bullet.radius + enemy.radius)
 end
 
@@ -54,35 +108,34 @@ function CollisionManager:checkShipEnemyCollision(ship, enemy)
     local dx = ship.x - enemy.x
     local dy = ship.y - enemy.y
     local distance = math.sqrt(dx * dx + dy * dy)
-    
+
     return distance < (ship.radius + enemy.radius)
 end
 
 function CollisionManager:bulletAndEnemyCollided(bullet, enemy)
     local isEnemyDead = enemy:takeDamage()
-    if isEnemyDead then 
-      createExplosion(enemy.x, enemy.y)
-      local collectable = spawnCollectable(enemy.x, enemy.y)
-      if collectable.type == "regular" then
-          gameController.instance:addPoints(enemy.points)
-      end
+    if isEnemyDead then
+        createExplosion(enemy.x, enemy.y)
+        local collectable = spawnCollectable(enemy.x, enemy.y)
+        if collectable.type == "regular" then
+            GameController.instance:addPoints(enemy.points)
+        end
     end
-    bulletSpawner.instance:delete(bullet)      
+    BulletSpawner.instance:delete(bullet)
 end
 
 function CollisionManager:shipAndEnemyCollided(ship, enemy)
     ship:takeDamage()
-    if gameController.instance:getLives() <= 0 then
+    if GameController.instance:getLives() <= 0 then
         createExplosion(enemy.x, enemy.y)
     end
     enemy.isDead = true
-    --table.remove(enemies, i)
 end
 
 function spawnCollectable(x, y)
     local asset = AssetsManager.sprites.coin
     local collectable = Collectable:init(x, y, asset)
-    
+
     local chance = math.random()
     if chance <= 0.1 then
         local powerUpType = Model.powerUpTypes[math.random(#Model.powerUpTypes)]
@@ -90,9 +143,9 @@ function spawnCollectable(x, y)
         collectable.asset = AssetsManager.sprites[powerUpType.assetName]
         collectable.duration = powerUpType.duration
     else
-        collectable.type = "regular" 
+        collectable.type = "regular"
     end
-    
+
     table.insert(collectables, collectable)
     return collectable
 end
@@ -100,11 +153,11 @@ end
 function CollisionManager:checkCollectableShipCollision(ship)
     for i = #collectables, 1, -1 do
         local collectable = collectables[i]
-        
+
         local dx = ship.x - collectable.x
         local dy = ship.y - collectable.y
         local distance = math.sqrt(dx * dx + dy * dy)
-        
+
         if distance < (ship.collectingRadius + collectable.radius) then
             collectable.isCollected = true
             ship:collect(collectable)
@@ -112,8 +165,7 @@ function CollisionManager:checkCollectableShipCollision(ship)
     end
 end
 
-
-function CollisionManager:updateCollectables(collectables, ship, dt) --collectables go to player
+function CollisionManager:updateCollectables(collectables, ship, dt)
     local speed = 500
 
     for i = #collectables, 1, -1 do
@@ -121,18 +173,18 @@ function CollisionManager:updateCollectables(collectables, ship, dt) --collectab
         if collectable.isCollected then
             local dx = ship.x - collectable.x
             local dy = ship.y - collectable.y
-            
+
             local angle = math.atan2(dy, dx)
-            
+
             collectable.x = collectable.x + math.cos(angle) * speed * dt
             collectable.y = collectable.y + math.sin(angle) * speed * dt
-            
+
             if math.abs(dx) < 5 and math.abs(dy) < 5 then
-                gameController.instance:addGold(1)
+                GameController.instance:addGold(1)
                 table.remove(collectables, i)
             end
         elseif collectable.y > Model.stage.stageHeight then
-             table.remove(collectables, i)
+            table.remove(collectables, i)
         else
             collectable:update(dt)
         end
@@ -143,7 +195,7 @@ function createExplosion(x, y)
     table.insert(explosions, {
         x = x,
         y = y,
-        timer = 0.5, 
+        timer = 0.5,
         asset = AssetsManager.sprites.explosion,
     })
 end
@@ -151,7 +203,7 @@ end
 function updateExplosions(dt)
     for i = #explosions, 1, -1 do
         local explosion = explosions[i]
-        explosion.timer = explosion.timer - dt 
+        explosion.timer = explosion.timer - dt
 
         if explosion.timer <= 0 then
             table.remove(explosions, i)
@@ -165,7 +217,7 @@ function CollisionManager:draw()
         local h = explosion.asset:getHeight()
         love.graphics.draw(explosion.asset, explosion.x - w / 2, explosion.y - h / 2)
     end
-    
+
     for _, collectable in ipairs(collectables) do
         collectable:draw()
     end
